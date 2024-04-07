3fd26c060487c292e9f6b3290c926e95a33f27b4
RamaMusic <ramamusicgo@gmail.com>
2024-04-06 23:35:08 -0300

    going back to original state

--- tp2/tp2.c
+++ tp2/tp2.c
@@ -23,70 +23,23 @@ struct list_iter {
 };
 
 list_t *list_new(){
-    list_t* list = malloc(sizeof(list_t));
-    if (list == NULL) {
-        return NULL;
-    }
-    list->head = NULL;
-    list->tail = NULL;
-    list->size = 0;
-    return list;
+    return NULL;
 }
 
 size_t list_length(const list_t *list){
-    if (list == NULL) {
-        return 0;
-    }
-    return list->size;
+    return 0;
 }
 
 bool list_is_empty(const list_t *list){
-    if (list == NULL) {
-        return true;
-    }
-    return list->size == 0;
+    return true;
 }
 
 bool list_insert_head(list_t *list, void *value){
-    if (list == NULL) {
-        return false;
-    }
-    node_t* node = malloc(sizeof(node_t));
-    if (node == NULL) {
-        return false;
-    }
-    node->value = value;
-    node->next = list->head;
-    node->prev = NULL;
-    if (list->head != NULL) {
-        list->head->prev = node;
-    }
-    list->head = node;
-    list->tail = list->tail == NULL ? node : list->tail;
-    list->size++;
-    return true;
+    return false;
 }
 
 bool list_insert_tail(list_t *list, void *value){
-    if (list == NULL) {
-        return false;
-    }
-    node_t* node = malloc(sizeof(node_t));
-    if (node == NULL) {
-        return false;
-    }
-    node->value = value;
-    node->next = NULL;
-    node->prev = list->tail;
-    if (list->tail != NULL) {
-        list->tail->next = node;
-    }
-    list->tail = node;
-    if (list->head == NULL) {
-        list->head = node;
-    }
-    list->size++;
-    return true;
+    return false;
 }
 
 void *list_peek_head(const list_t *list){
