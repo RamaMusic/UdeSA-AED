b423ce2626169d5bd730d2c74719f2debfaa563f
RamaMusic <ramamusicgo@gmail.com>
2024-04-07 01:53:04 -0300

    idk, progress i guess

--- tp2/tp2.c
+++ tp2/tp2.c
@@ -23,43 +23,102 @@ struct list_iter {
 };
 
 list_t *list_new(){
-    return NULL;
+    list_t* list = malloc(sizeof(list_t));
+    if (list == NULL) return NULL;
+    list->head = NULL;
+    list->tail = NULL;
+    list->size = 0;
+    return list;
 }
 
 size_t list_length(const list_t *list){
-    return 0;
+    if (list == NULL) return 0;
+    return list->size;
 }
 
 bool list_is_empty(const list_t *list){
-    return true;
+    if (list == NULL) return NULL;
+    return list->size == 0;
 }
 
 bool list_insert_head(list_t *list, void *value){
-    return false;
+    if (list == NULL) return false;
+    
+    node_t* node = malloc(sizeof(node_t));
+    if (node == NULL) return false;
+
+    node->prev = NULL;
+    node->next = list->head;
+    node->value = value;
+
+    if (list->head != NULL) list->head->prev = node;
+    if (list->tail == NULL) list->tail = node;
+    list->head = node;
+    list->size++;
+
+    return true;
 }
 
 bool list_insert_tail(list_t *list, void *value){
-    return false;
+    if (list == NULL) return false;
+
+    node_t* node = malloc(sizeof(node_t));
+    if (node == NULL) return false;
+    node->value = value;
+    node->next = NULL;
+    node->prev = list->tail;
+
+    if (list->tail != NULL) list->tail->next = node;
+    if (list->head == NULL) list->head = node;
+    list->tail = node;
+    list->size++;
+    return true;
 }
 
 void *list_peek_head(const list_t *list){
-    return NULL;
+    if (list == NULL || list->head == NULL) return NULL;
+    return list->head->value;
 }
 
 void *list_peek_tail(const list_t *list){
-    return NULL;
+    if (list == NULL || list->tail == NULL) return NULL;
+    return list->tail->value;
 }
 
 void *list_pop_head(list_t *list){
-    return NULL;
+    if (list == NULL || list->head == NULL) return NULL;
+
+    node_t* node = list->head->next;
+    void* value = list->head->value;
+
+    if (node != NULL) node->prev = NULL; else list->tail = NULL;
+
+    free(list->head);
+    list->head = node;
+
+    list->size--;
+    return value;
 }
 
 void *list_pop_tail(list_t *list){
-    return NULL;
+    if (list == NULL || list->tail == NULL) return NULL;
+
+    node_t* node = list->tail->prev;
+    void* value = list->tail->value;
+
+    if (node != NULL) node->next = NULL; else list->head = NULL;
+
+    free(list->tail);
+    list->tail = node;
+
+    list->size--;
+    return value;
 }
 
 void list_destroy(list_t *list, void destroy_value(void *)){
-    return;
+    if (list == NULL) return;
+
+    // qué
 }
 
 list_iter_t *list_iter_create_head(list_t *list){
